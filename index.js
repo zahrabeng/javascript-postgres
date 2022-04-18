b746c1a8b546caa651b2805546ee57f404ba0c54
Zahra <bghzahra@outlook.com>
2022-04-18 11:59:28 +0100

    added parametes of kind movie in a SQL injection safe way

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const client = new Client({
 })
 
 client.connect().then(()=> console.log("Connected Successfully")) //promise that gets fulfilled when client is connected
-.then(()=> client.query("SELECT * FROM movies LIMIT 10"))
+.then(()=> client.query("SELECT * FROM movies WHERE kind=$1 LIMIT 10 ", ["movie"]))
 .then(results => console.log(results.rows))
 .catch(e => console.log(e)) // shows error if promise is rejected
 .finally(()=> client.end)  //ends the connection 
