01013ed3bee1f47705a18804e2cd76271ab769fa
Zahra <bghzahra@outlook.com>
2022-04-18 12:36:58 +0100

    added client objecr

--- async.js
+++ async.js
@@ -1,1 +1,11 @@
-const {Client} = require('pg')
+const {Client, Client} = require('pg')
+const Client = new Client({
+    user:"academy",
+    password: process.env.password,
+    database: "omdb"
+})
+
+
+async function excecute(){
+
+}
